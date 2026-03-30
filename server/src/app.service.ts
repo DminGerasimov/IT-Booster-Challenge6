49537c7daf7ba3a4a4039ea8c61ba108d417749b
Dmitry <nsmg@yandex.ru>
2026-03-30 11:53:29 +0300

    Fix issue with pdf template

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -7,7 +7,7 @@ export class AppService {
   constructor(private readonly queueService: QueueService) {}
 
   async createJob(jobPayload: string) {
-    const jobId = await this.queueService.addJob({ text: jobPayload });
+    const jobId = await this.queueService.addJob(jobPayload);
     console.log(`Job #${jobId} created`);
     console.log(`Job payload: ${jobPayload}`);
   }

87875f967537973c0678cf49293d7ef0cd82fa40
Dmitry <nsmg@yandex.ru>
2026-03-30 10:40:39 +0300

    Add file downloader controller

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { createReadStream } from 'fs';
+import { join } from 'path';
+
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Res,
+  StreamableFile,
+} from '@nestjs/common';
 
 import { AppService } from './app.service';
 import { TextDto } from './app.text.dto';
@@ -15,5 +25,13 @@ export class AppController {
   @Get('/checkjobs')
   async checkJobs() {
     return await this.appService.checkJobs();
+  }
+
+  @Get('/download')
+  downloadFile(@Res({ passthrough: true }) response: Response): StreamableFile {
+    const file = createReadStream(join(process.cwd(), '1.pdf'));
+    console.log(response.headers);
+    console.log(process.cwd());
+    return new StreamableFile(file);
   }
 }
